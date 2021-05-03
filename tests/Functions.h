eb962a321f2c4c555bfaa7cf91c7d62a4473fa9a
iamiqbal <muhammadiqbal2015@hotmail.com>
2021-05-03 18:15:51 +0500

    Euler Method and test functions

---
+++ tests/Functions.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+
+// y(0) = 0, h = 0.5
+// 0 <= t <= 1
+double f1(double y, double t){
+    return (t * exp(3 * t)) - 2 * y;
+}
+
+// y(2) = 1, h = 0.5
+// 2 <= t <= 3
+double f2(double y, double t){
+    return 1 + (pow((t-y), 2));
+}
+
+// y(1) = 2, h = 0.25
+// 1 <= t <= 2
+double f3(double y, double t){
+    return 1 + (y / t);
+}
+
+// y(0) = 1, h = 0.25
+// 0 <= t <= 1
+double f4(double y, double t){
+    return cos(2 * t) + sin(3 * t);
+}
