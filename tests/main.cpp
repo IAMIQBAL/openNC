38eb58479d55cb5cec49c56a67813e6d469da55f
iamiqbal <muhammadiqbal2015@hotmail.com>
2021-05-10 22:37:59 +0500

    fixes

--- tests/main.cpp
+++ tests/main.cpp
@@ -7,27 +7,29 @@ namespace plt = matplotlibcpp;
 
 int main(){
 
-    Euler eu(0.0, 2.0, 0.2, 0.5);
+    Euler eu(0.0, 2.0, 0.1, 0.5);
     double **tw;
     tw = eu.approximate();
 
-    int n = (2.0 - 0.0) / 0.2;
+    int n = (2.0 - 0.0) / 0.1;
 
-    std::vector<double> t(n);
-    std::vector<double> w(n);
+    std::vector<double> t(n+1);
+    std::vector<double> w(n+1);
 
     std::cout << "i\t\t" << "t\t\t" << "w" << std::endl;
 
-    for (int i = 1; i <= n; i++){
+    for (int i = 0; i <= n; ++i){
         std::cout << i << "\t\t" << tw[0][i] << "\t\t" << tw[1][i];
         std::cout << std::endl;
         t[i] = tw[0][i];
         w[i] = tw[1][i];
     }
 
-    plt::xkcd();
-    plt::plot(t, w);
+    plt::plot(t, w, "r-o");
     plt::title("Euler`s Method");
+    plt::xlabel("t");
+    plt::ylabel("w");
+    plt::grid(1);
     plt::show();
     
     return 0;
